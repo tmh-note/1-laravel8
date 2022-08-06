8d66a0497be7938b172ef1de0a50006183235fb3
Thet Minn Htun <59432845+thetminnhtun-scm@users.noreply.github.com>
2022-08-06 18:40:43 +0630

    Install laravel ui

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('product', [ProductController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
